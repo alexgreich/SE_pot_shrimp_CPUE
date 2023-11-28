51bff573cc3143a6ddabf5b6b9e05fad447b2998
agreich <alex.reich@alaska.gov>
2023-11-27 15:58:40 -0900

    git-ing there...

--- Exploring_the_data_2.R
+++ Exploring_the_data_2.R
@@ -20,11 +20,98 @@ library(corrplot)
 #load data  -> IN SUBSEQUENT VERSIONS, THIS SHOULD SOURCE DIRECTLY FROM ORACLE
 ###there have been...frustrating complications and I'm working on it (medium priority)
 ###################
+#intially, I'm looking at Fish Ticket and/or Logbook data just from Ernest Sound 
+fish_tickets_ernest <- read.csv("Data/Ernest_sound_fish_tickets.csv")
+
+
+names(fish_tickets_ernest)
+#Pounds..sum and Whole.Weight..sum. should not be identical?
+identical(fish_tickets_ernest$Pounds..sum., fish_tickets_ernest$Whole.Weight..sum.)
+#yup, they're identical
+
+#anyway...
+
+
 
 ##################################
 #The data wrangle section
 ###############################
 
+###wrangle for my focus years
+unique(fish_tickets_ernest$Season.Ref) # we want 01-02 and higher for now
+
+focus_years_ernest <- fish_tickets_ernest %>% filter(Season.Ref %in% c("01-02", "02-03", "03-04", "04-05", "05-06", "06-07",
+                                                                       "07-08", "08-09", "09-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20",
+                                                                       "20-21", "21-22", "22-23")) #select for the seasons I want here
+unique(focus_years_ernest$Season.Ref)
+
+##make sure I only have pot (9 or 91) gear and only have spot shrimp (for this stat area)
+unique(focus_years_ernest$Gear.Code) #just 91, indicating pots. Good
+
+unique(focus_years_ernest$Species.Code) #there's multiple here. We just want spot shrimp (965)
+#focus_years_ernest <- focus_years_ernest %>% filter(Species.Code == 965) NOT YET OR ELSE POTS MIGHT SAY 0!!
+
+
+###select just the columns that I am interested in for this analysis
+focus_years_ernest <- focus_years_ernest %>% select(Batch.Year, Pre.Print.Ticket, Fish.Ticket.Number, ADFG.Number, Vessel.Name, Event.Date, DOL.Month, Stat.Week, Date.of.Landing, Season, Season.Ref,
+                                                    Item.Number, 
+                                                    Stat.Area, Whole.Weight..sum., Pots, Species.Code, Batch.Year) #what is item number? the item per fish ticket
+
+###wrangle so that I have a new column called Number_of_vessels, which will represent the number of vessels in a current season
+focus_years_ernest <- focus_years_ernest %>% 
+  group_by(Season.Ref) %>% #grouping by season (year)
+  mutate(vessel_count = n_distinct(ADFG.Number)) %>% #count the unique # of vessels (by ADFG number)
+  ungroup() #ungroup
+
+
+###wrangle: sum the weight(sum) per each fish ticket, while keeping the MAX of pots for that fish ticket. This will insure that pots are not counted twice/multiple times
+#there are some NA's but I dont want to deal with them
+
+sum_shrimp_ernest <- focus_years_ernest %>%
+  mutate(Pots = replace_na(Pots, 0)) %>%
+  group_by(Fish.Ticket.Number, Species.Code) %>% #also group by stat area for analysis areas with multiple stat areas.
+  summarise(
+    total_weight = sum(Whole.Weight..sum.),
+    max_pots = max(Pots),
+    ADFG.Number = max(ADFG.Number),
+    Season.Ref=max(Season.Ref),
+    Vessel.Name = max(Vessel.Name),
+    DOL.Month= max(DOL.Month),
+    Stat.Week=max(Stat.Week), 
+    Batch.Year=max(Batch.Year), #add Event date, date of landing, or vessel count here? #change from V1 ... vessel count is a continuous variable (integer... GAM time?)
+    Event.Date=max(Event.Date),
+    vessel_count=max(vessel_count) #that should do it.
+  ) %>%
+  ungroup() #this has pots and total weight for all shrimp species
+
+max_pots_total <- sum_shrimp_ernest %>% #get # pots fpr each fish ticket
+  group_by(Fish.Ticket.Number) %>%
+  summarise(
+    max_pots_2 = max(max_pots)
+  )%>%
+  ungroup()
+
+sum_spot_shrimp_ernest <- sum_shrimp_ernest %>%
+  filter(Species.Code == 965) %>%
+  right_join(max_pots_total) %>%
+  select(-max_pots)
+
+#View(sum_spot_shrimp_ernest)
+#str(sum_shrimp_ernest)
+#str(focus_years_ernest)
+
+
+#calculate nominal CPUE
+sum_spot_shrimp_ernest <- sum_spot_shrimp_ernest %>%
+  filter(max_pots_2 != 0) %>%
+  mutate(CPUE_nom = total_weight/max_pots_2)
+
+##QC: is it possible that there are one pot # entry for multiple stat areas? If so, more data wrangling required (to make sure there is not a 0 where there should not be a 0)
+#Q: do seasons need to be in order??
+
+
+#WRANGLE.EVENT.DATE.TO.JULIAN.DATE.PLEASE!!!!!!!!!!!!!!!!!:
+###wrangle wrangle here.###
 
 
 ###########################
@@ -33,8 +120,19 @@ library(corrplot)
 
 
 
+names(sum_spot_shrimp_ernest)
+
+corr_spot <- sum_spot_shrimp_ernest %>% #I want to see the correlations graphed
+  select(ADFG.Number, Season.Ref, DOL.Month, Stat.Week, CPUE_nom, Batch.Year, Event.Date, vessel_count) %>%
+  mutate(ADFG.Number = factor(ADFG.Number), Season.Ref = as.factor(Season.Ref)) 
 
 
+corr_spot2 <- sum_spot_shrimp_ernest %>% #a closer look
+  select(DOL.Month, Stat.Week, CPUE_nom, Batch.Year,vessel_count)
+
+#corrilation plots
+#pairs(corr_spot) #argh it does not like non numberic argulemts
+pairs(corr_spot2)
 
 ###############################
 #the analysis section
