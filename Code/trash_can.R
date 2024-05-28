c13a52fc12061ffa80273843f9be582690dc4cad
agreich <alex.reich@alaska.gov>
2024-05-28 14:04:17 -0800

    imputing, several methods, sigh

--- Code/trash_can.R
+++ Code/trash_can.R
@@ -295,3 +295,192 @@ summary(M15)
 AIC(M12, M15)
 
 ```
+
+#DISTRICT 7
+
+
+
+
+Some wild experimentation below,
+delete later and keep what is useful
+```{r}
+#null model
+m0 <- gam(log(CPUE_nom + 0.001) ~ factor(Season.Ref) ,data= D7, method="ML" ) #how to add temporal autocorrealtion to this tho?
+
+m_glob_r <- gam(log(CPUE_nom + 0.001) ~ Season.Ref + s(ADFG.Number, bs="re")+ s(vessel_count_mgmt_u, k=4) + s(jdate, k=4) + s(Analysis.Area, bs="re", k=4), data=D7 , method="ML") 
+
+m_glob_r_2 <- gam(log(CPUE_nom + 0.001) ~ Season.Ref+ ADFG.Number + s(vessel_count_mgmt_u, k=4) + s(jdate, k=4) + s(Analysis.Area, bs="re", k=4), data=D7, method="ML" )
+
+m_glob_nr <- gam(log(CPUE_nom + 0.001) ~ Season.Ref + ADFG.Number+ s(vessel_count_mgmt_u, k=4) + s(jdate, k=4), data=D7, method="ML") 
+
+#do I need random effects in the model, and which random effects?
+AIC(m_glob_r, m_glob_r_2, m_glob_nr, m0) #m glob r wins, but not by much
+
+
+#get rid some variables
+summary(m_glob_r)
+m_3 <- gam(log(CPUE_nom + 0.001) ~ Season.Ref + s(ADFG.Number, bs="re")+ s(jdate, k=4) + s(Analysis.Area, bs="re", k=4), data=D7, method="ML" ) 
+
+AIC(m_glob_r, m_3) #same, meaning drop vessel count
+summary(m_3)
+
+#oh shit tho, let me check temporal autocorrelation, maybe I should check for interactions
+?acf
+acf(residuals(m_glob_r),main="raw residual ACF") #yeah, some autocorrelation
+
+#so...what do I do?
+
+#try the global model with interaction effects, see if autocorrelation still exists
+#https://r.qcbs.ca/workshop08/book-en/gam-with-interaction-terms.html
+#m_glob_r_int <- gam(log(CPUE_nom + 0.001) ~ Season.Ref + s(ADFG.Number, bs="re")+ s(vessel_count_mgmt_u, k=4) + s(jdate, k=4) + s(Analysis.Area, bs="re", k=4), data=D7 , method="ML") +
+ #s() #interaction effects
+
+#ok ummm, maybe do smaller levels first to chekc for int
+mod_int_1 <- gam(log(CPUE_nom + 0.001) ~ Season.Ref + s(ADFG.Number, bs="re")+ s(vessel_count_mgmt_u, k=4) + s(vessel_count_mgmt_u, by=Season.Ref, k=4) + s(Analysis.Area, bs="re", k=4), data=D7 , method="REML") #
+
+summary(mod_int_1)
+summary(m_glob_r)
+
+
+```
+
+
+But wait do I even, like, need a GAM?
+m_glob_r <- gam(log(CPUE_nom + 0.001) ~ Season.Ref + s(ADFG.Number, bs="re")+ s(vessel_count_mgmt_u, k=4) + s(jdate, k=4) + s(Analysis.Area, bs="re", k=4), data=D7 , method="ML") 
+```{r}
+#remove outleir
+max(D7$CPUE_nom)
+D7_no_o <- D7 %>% filter(CPUE_nom < 100)
+
+#log cpue vs. ADFG number (ok thats a radom effect)
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=ADFG.Number) + geom_boxplot()
+###oh maybe this should be a special random effect. I forget
+##this: Random factor smooth interactions
+#bs="fs" A special smoother class (see smooth.construct.fs.smooth.spec) is available for the case in which a smooth is required at each of a large number of factor levels (for example a smooth for each patient in a study), and each smooth should have the same smoothing parameter. The "fs" smoothers are set up to be efficient when used with gamm, and have penalties on each null sapce component (i.e. they are fully ‘random effects’). from: https://stat.ethz.ch/R-manual/R-patched/RHOME/library/mgcv/html/smooth.terms.html
+#or just a regular random effect. can try both ways. why not
+
+#log cpue vs. season ref (is this smoothed in phil's code??)
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Season.Ref) + geom_boxplot(outliers=F) #totally autocorrelated
+
+#log cpue vs. Analysis area (also a random effect)
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Analysis.Area) + geom_boxplot() #ok they're not... that different
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Analysis.Area) + geom_boxplot(outliers=F)
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Analysis.Area) + geom_violin()
+
+ggplot(D7) + aes(y= CPUE_nom, x=Analysis.Area) + geom_boxplot(outliers=F)
+ggplot(D7) + aes(y= CPUE_nom, x=Analysis.Area) + geom_violin()
+
+#log cpue vs jdate (is this worth including, because the season might not be so important to look at, migjt give uneven results since fishing season recently shifted)
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=jdate) + geom_smooth() #i think maybe jdate isnt necessary because the fishing season is a specific window.
+
+#log cpue vs. vessel count
+##vessel count by year and analysis area
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=vessel_count_aa) + geom_point() + geom_smooth()
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=vessel_count_aa) + geom_smooth() + geom_point() #ok that puts thigs in perspective
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=vessel_count_aa) + geom_smooth(method = "lm")# + geom_point()
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=vessel_count_aa) + geom_smooth(method = "lm") + geom_point()
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=vessel_count_aa) + geom_point()
+
+ggplot(D7_no_o) + aes(y= CPUE_nom, x=vessel_count_aa) +geom_point()+ geom_smooth()
+ggplot(D7_no_o) + aes(y= CPUE_nom, x=vessel_count_aa, group=vessel_count_aa) +geom_boxplot()+ geom_smooth()
+ggplot(D7_no_o) + aes(y= CPUE_nom, x=vessel_count_aa, color=Season.Ref) +geom_point()
+ggplot(D7) + aes(y= CPUE_nom, x=vessel_count_aa) + geom_smooth(method = "lm") #ok teh change in cpue is super small. Like, 1 pound. Idbutk if that matters 
+ggplot(D7_no_o) + aes(y= CPUE_nom, x=vessel_count_aa) + geom_smooth(method = "lm") + geom_point()
+
+##vessel count by year and mgmt u
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=vessel_count_mgmt_u) + geom_smooth()
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=vessel_count_mgmt_u) + geom_point()
+
+
+#investigate relationships for random effects
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Season.Ref) + geom_point()+ facet_wrap(~Analysis.Area)
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Season.Ref, color=Analysis.Area) + geom_boxplot() + theme_cowplot() # i would like to do a geom smooth plot of this too
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Season.Ref, color=Analysis.Area) + geom_violin() + theme_cowplot()
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Season.Ref, color=ADFG.Number) + geom_boxplot() + theme_cowplot() + theme(legend.position= "none")
+
+##your experience depends on what group you are inb
+
+#what is an average fishing vessel that is in all locations
+ggplot(D7) + aes(y= log(CPUE_nom+0.001), x=Analysis.Area, color=ADFG.Number) + geom_boxplot() + theme_cowplot() #+ theme(legend.position= "none")
+
+
+```
+
+
+
+
+So, I wanted a model without smoothed effects. I probs should not use the gam function, but nlme or lme4 or something
+Test if ranefs are necessary, please
+It should be a glmm I think its a glmm. 4/29/24
+- shoule ADFG.number be random or fixed effect????
+  - year should be fixed effect
+- area should be random effect
+- do year and area have interaction effects? Do year and vessel have interaction effects? do vessel and year have interaction effects?
+  --what is the best ranef structure??
+  - correlation matrix include??
+  - test vessel count (aa) as fixef too.
+- maybe add week instead of jdate??
+  read the highlighted refs in the mauder and punt paper
+```{r}
+library(lme4)
+library(lmerTest)
+
+
+mod_g_new <- gam(log(CPUE_nom+0.001) ~ Season.Ref + s(vessel_count_aa, k=4) + s(Analysis.Area, bs="re") + s(ADFG.Number, bs="re"), method = "ML", data=D7)
+summary(mod_g_new)
+#the smoothed model, for funzies
+
+mod_g_2 <- gam(log(CPUE_nom+0.001) ~ Season.Ref + s(Analysis.Area, bs="re") + s(ADFG.Number, bs="re"), method = "ML", data=D7)
+
+AIC(mod_g_new, mod_g_2) #the more complex one wins(with vessel cound as a smoother). I do not undersand tho, why we would want to make vessel count smooth
+
+
+#does vessel count matter? I do not think so.
+#do area and year have interaction effects
+#test just with lm for above
+#do area and vessel have interaction effects
+
+#global model - writing these, need sun
+#m_interactions
+#m_glob <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + vessel_count_aa + (1|ADFG.Number) + (1+ Season.Ref|Analysis.Area), data=D7, REML=F) #not run, takes forevet
+#m_glob_0.1 <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + vessel_count_aa + (1 + Season.Ref|ADFG.Number) + (1+ Season.Ref|Analysis.Area), data=D7, REML=F) #not run, takes forever
+#takes forever
+
+m_glob2 <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + ADFG.Number + (1|Analysis.Area), data=D7, REML=F)
+m_glob3 <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + (1|ADFG.Number) + (1|Analysis.Area), data=D7, REML=F)
+m_glob4 <- lm(log(CPUE_nom+0.001) ~ Season.Ref + ADFG.Number + Analysis.Area, data=D7)
+m_glob5 <- lm(log(CPUE_nom+0.001) ~ Season.Ref + ADFG.Number + Analysis.Area + vessel_count_aa, data=D7)
+#m_glob5.2 <- lm(log(CPUE_nom+0.001) ~ Season.Ref * ADFG.Number * Analysis.Area * vessel_count_aa, data=D7) #takes forever
+m_glob6 <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + vessel_count_aa + (1|ADFG.Number) + (1|Analysis.Area), data=D7, REML=F) #gonna want to graph correlations
+m_glob6_I <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + vessel_count_aa + Season.Ref:vessel_count_aa + (1|ADFG.Number) + (1|Analysis.Area), data=D7, REML=F)
+m_glob7 <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + vessel_count_aa + ADFG.Number + (1|Analysis.Area), data=D7, REML=F)
+#m_glob7_I <- lmer(log(CPUE_nom+0.001) ~ Season.Ref * vessel_count_aa * ADFG.Number + (1|Analysis.Area), data=D7, REML=F) #takes forever
+m_glob8 <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + vessel_count_aa + (1|ADFG.Number) + Analysis.Area, data=D7, REML=F)
+
+AIC(m_glob2, m_glob3, m_glob4,m_glob5,  m_glob6, m_glob6_I, m_glob7, m_glob8) #says glob 5. Let's chgeck for correlation tho. 6 is better than 3, if we choose ranef
+summary(m_glob2)
+summary(m_glob4)
+summary(m_glob5) #the AIC prefers the linear model
+summary(m_glob6) #but conceptually, I think we should go with this model
+summary(m_glob6_I)
+#vcov(m_glob6_I)
+
+#based on theory, this is what I think the model should be:
+##m_glob6, m_glob6_I, or m_glob7. 
+AIC(m_glob6, m_glob6_I, m_glob7) #m_glob7_I appears to take forever
+BIC(m_glob6, m_glob6_I, m_glob7, m_glob5) #syas 7 and 5 are close to each other
+library(MuMIn)
+AICc(m_glob6)
+AICc(m_glob6_I)
+AICc(m_glob7)
+AICc(m_glob5) #need to account for those interaction effects tho
+
+
+#REML the chosen model before mocing on
+##m_glob6_I makes sense to me theoretically, even tho it does not win by model selection numbers. I should consider making ADFG number a fixed effect tho
+
+m_glob6_IR <- lmer(log(CPUE_nom+0.001) ~ Season.Ref + vessel_count_aa + Season.Ref:vessel_count_aa + (1|ADFG.Number) + (1|Analysis.Area), data=D7, REML=T) #season ref could be year. Would it make a difference?
+
+
+
+```
